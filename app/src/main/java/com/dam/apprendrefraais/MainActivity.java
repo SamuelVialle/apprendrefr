e3197c74bef790bbad44892e9968384b4e9731f7
jasem <jasem19876@hotmail.com>
2022-12-01 16:55:21 +0100

    bonjour

--- app/src/main/java/com/dam/apprendrefraais/MainActivity.java
+++ app/src/main/java/com/dam/apprendrefraais/MainActivity.java
@@ -2,6 +2,7 @@ package com.dam.apprendrefraais;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MediaPlayer mp= new MediaPlayer();
+        try{
+            mp.setDataSource();
     }
-}
